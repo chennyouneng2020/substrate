abd3fe829bedc02afa0bbe77d09da05205661e1b
chennyouneng2020 <61309554+chennyouneng2020@users.noreply.github.com>
2021-04-01 20:39:04 +0800

    Update main.rs

--- class4_2/src/main.rs
+++ class4_2/src/main.rs
@@ -1,16 +1,19 @@
-fn Sum(vec: &Vec<u32>)-> Option<u32> {
+
+//计算集合的和
+fn Sum(vec: &[u32])-> Option<u32> {
     let mut sums: u32=0;
     for i in vec{
        match  sums.checked_add(*i) {
-            Some(s) => {sums=sums+s;}
+            Some(s) => {sums=s;}
             None => {return None;}  
        };
     }
     Some(sums)
 }
 
+
 fn main(){
     let a=vec![12,33,20,333,84];
-    println!("{:?}",Sum(&a));
+     println!("sum ={:?}",sum);
 
 }
